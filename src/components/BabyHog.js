d97dd1e33e8ff6cde26c8de8a6a495bbb0ac7ed4
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-04 11:13:00 -0500

    completed assignment

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import normalBaby from "../assets/unadulterated-hoglette.png";
 import SunBaby from "../assets/sun-eyes.png";
 import BlueBaby from "../assets/blue-eyes.png";
@@ -8,24 +8,39 @@ import GlowingBaby from "../assets/glowing-eyes.png";
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
 function BabyHog(props) {
-  function handleChangeWeight(e) {
+  const [weight, setWeight] = useState(200);
+
+  function handleIncrease(e) {
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
+    setWeight(e.target.name === "+" ? weight + 20 : weight - 20)
+  }
+  function eyeColorMapper(color) {
+    if (color === "blue") {
+      return BlueBaby
+    }
+    else if (color === "glowing") {
+      return GlowingBaby
+    }
+    else if (color === "sun") {
+      return SunBaby
+    }
+    else { return normalBaby }
   }
 
   return (
     <li className="hogbabies">
-      <h1>Name</h1>
-      <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
+      <h1>Name:{props.name}</h1>
+      <h3>Weight:{weight}:</h3>
+      <h3>Hobby:{props.hobby}</h3>
+      <h4>Eye Color:{props.eyeColor}</h4>
 
-      <button name="+">Increase Weight</button>
-      <button name="-">Decrease Weight</button>
+      <button name="+" onClick={handleIncrease}>Increase Weight</button>
+      <button name="-" onClick={handleIncrease}>Decrease Weight</button>
 
       <div className="hb-wrap">
         <img
-          src={normalBaby}
+          src={eyeColorMapper(props.eyeColor)}
           style={{ height: "200px" }}
           alt="MasterBlasterJrJr"
         />
